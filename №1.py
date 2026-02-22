c224ac070b60658be348702fc624fe18aa835a73
deniskadyn <deniskadyn82@gmail.com>
2026-02-22 20:52:13 +0300

    Add files via upload

---
+++ №1.py
@@ -0,0 +1,5 @@
+name = input("Ваше имя: ")
+surname = input("Фамилия: ")
+age = input("Возраст: ")
+print("Ваше имя: {}, Фамилия: {}, Возраст: {} лет".format(name, surname, age))
+print(f"Ваше имя: {name}, Фамилия: {surname}, Возраст: {age} лет")
